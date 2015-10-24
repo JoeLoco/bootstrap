f723d0c7e3b11cedc6f119c99beae97933b6e94a
unknown <joao.santos@imob.io>
2015-10-24 15:12:39 -0200

    creating readme

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 ## A bootstrap 3 helper for Laravel 5
 
-:-)
+# Install
+
+```console
+composer install joeloco/bootstrap
+```
+
+# Config
+
+1 - Register the service provider in /config/app 
+
+```php
+'providers' => [
+        //.....
+        Bootstrap\Providers\BootstrapServiceProvider::class
+    ],
+```
+
+2 - Set a alias for facade in /config/app 
+
+```php
+
+'aliases' => [
+        //....
+        'BS' => Bootstrap\Facades\Bootstrap::class,
+    ],
+```
+
+# Breadcrumb example
+
+```php
+<?php echo BS::breadcrumb()->drop('Root','/root')->drop('Chield','root/chield')->stop('right');?>
+```
+
+
+
+
--- src/Bootstrap/Breadcrumb.php
+++ src/Bootstrap/Breadcrumb.php
@@ -7,13 +7,38 @@ namespace Bootstrap;
  */
 class Breadcrumb {
     
-    public function drop()
+    private $items = array();
+
+    public function drop($label, $url = null)
     {
-        
+        $this->items[] = array("label" => $label, "url" => $url);
+        return $this;
     }
     
-    public function stop()
+    public function stop($right = null)
     {
+        $output = "";
+        $output .= "<ul class='breadcrumb'>";
         
+        $last = array_pop($this->items); 
+        foreach ($this->items as $item) {
+            if(empty($item["url"]))
+            {
+                $output .= "<li>".$item["label"]."</li>";
+            }
+            else
+            {
+                $output .= "<li><a href='" . $item["url"] . "'>".$item["label"]."</a></li>";
+            }
+            
+        }
+        $output .= "<li class='last-item'>".$last["label"]."</li>";
+        if($right)
+        {
+            $output .= "<li class='pull-right'>{$right}</li>";
+        }
+        $output .= "</ul>";
+
+        return $output;
     }
 }
