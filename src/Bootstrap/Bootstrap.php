e56502ebcea9c737dfc55f4beb9ddc0f5fd31862
unknown <joao.santos@imob.io>
2015-10-24 13:49:17 -0200

    add main class

---
+++ src/Bootstrap/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+namespace Bootstrap;
+
+/**
+ * Create a bootstrap breadcrumb
+ * @author joeloco
+ */
+class Breadcrumb {
+    
+    public function breadcrumb()
+    {
+        return new Breadcrumb();
+    }
+    
+}
